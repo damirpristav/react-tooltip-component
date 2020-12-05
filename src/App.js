d2fbca665701385b13e687e16b4a456a8eae9a45
Damir Pristav <damirpristav@gmail.com>
2020-12-05 10:34:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Tooltip from './Tooltip';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tooltip position="top" content="lorem ipsum dolor sit amet" animationDuration={600}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, iste hic ducimus perspiciatis quae quibusdam voluptates repellendus reiciendis consequatur nulla necessitatibus velit ex. Earum cupiditate nesciunt saepe alias ad quas ex doloribus at quae amet optio iste facilis, eaque dicta. Maxime dolorem, enim ducimus natus perferendis dolor voluptate velit dignissimos.</p>
+      </Tooltip>
+      <Tooltip position="top" content="lorem ipsum">
+        <button>top</button>
+      </Tooltip>
+      <Tooltip position="bottom" content="lorem ipsum">
+        <button>bottom</button>
+      </Tooltip>
+      <Tooltip position="left" content="lorem ipsum">
+        <button>left</button>
+      </Tooltip>
+      <Tooltip position="right" content="lorem ipsum">
+        <button>right</button>
+      </Tooltip>
+      <Tooltip position="top-left" content="lorem ipsum">
+        <button>top left</button>
+      </Tooltip>
+      <Tooltip position="top-right" content="lorem ipsum">
+        <button>top right</button>
+      </Tooltip>
+      <Tooltip position="bottom-left" content="lorem ipsum">
+        <button>bottom left</button>
+      </Tooltip>
+      <Tooltip position="bottom-right" content="lorem ipsum">
+        <button>bottom right</button>
+      </Tooltip>
+      <Tooltip position="left-top" content="lorem ipsum">
+        <button>left top</button>
+      </Tooltip>
+      <Tooltip position="left-bottom" content="lorem ipsum">
+        <button>left bottom</button>
+      </Tooltip>
+      <Tooltip position="right-top" content="lorem ipsum">
+        <button>right top</button>
+      </Tooltip>
+      <Tooltip position="right-bottom" content="lorem ipsum">
+        <button>right bottom</button>
+      </Tooltip>
     </div>
   );
 }
---
+++ src/Tooltip.js
@@ -0,0 +1,200 @@
+import React, { Fragment, cloneElement, useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+// Content
+const TooltipContent = ({ tooltipClass, content, position, tooltipPosition, duration }) => {
+  const tooltipEl = useRef();
+  const targetEl = document.getElementById('root');
+
+  useEffect(() => {
+    const el = tooltipEl.current;
+
+    if(el) {
+      el.style.transitionDuration = duration + 'ms';
+
+      setTimeout(() => {
+        if(tooltipPosition === 'top') {
+          el.style.top = `${position.top - el.clientHeight}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(-50%, -15px) scale(1)`;
+        }
+        else if(tooltipPosition === 'bottom') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(-50%, 15px) scale(1)`;
+        }
+        else if(tooltipPosition === 'left') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left - el.clientWidth}px`;
+          el.style.transform = `translate(-15px, -50%) scale(1)`;
+        }
+        else if(tooltipPosition === 'right') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(15px, -50%) scale(1)`;
+        }
+        else if(tooltipPosition === 'top-left') {
+          el.style.top = `${position.top - el.clientHeight}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(0, -15px) scale(1)`;
+        }
+        else if(tooltipPosition === 'top-right') {
+          el.style.top = `${position.top - el.clientHeight}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(-100%, -15px) scale(1)`;
+        }
+        else if(tooltipPosition === 'bottom-left') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(0, 15px) scale(1)`;
+        }
+        else if(tooltipPosition === 'bottom-right') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(-100%, 15px) scale(1)`;
+        }
+        else if(tooltipPosition === 'left-top') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left - el.clientWidth}px`;
+          el.style.transform = `translate(-15px, 0) scale(1)`;
+        }
+        else if(tooltipPosition === 'left-bottom') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left - el.clientWidth}px`;
+          el.style.transform = `translate(-15px, -100%) scale(1)`;
+        }
+        else if(tooltipPosition === 'right-top') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(15px, 0) scale(1)`;
+        }
+        else if(tooltipPosition === 'right-bottom') {
+          el.style.top = `${position.top}px`;
+          el.style.left = `${position.left}px`;
+          el.style.transform = `translate(15px, -100%) scale(1)`;
+        }
+
+        el.style.opacity = '1';
+      }, 20);
+    }
+  }, []);
+
+  const output = <div className={tooltipClass} ref={tooltipEl}>{content}</div>
+
+  return targetEl ? ReactDOM.createPortal(output, targetEl) : output;
+}
+
+TooltipContent.propTypes = {
+  content: PropTypes.any.isRequired,
+  tooltipClass: PropTypes.string.isRequired,
+  position: PropTypes.shape({
+    top: PropTypes.number.isRequired,
+    left: PropTypes.number.isRequired
+  }).isRequired,
+  tooltipPosition: PropTypes.string.isRequired,
+  duration: PropTypes.number.isRequired
+}
+
+// Tooltip
+const Tooltip = ({ children, position, content, animationDuration = 200 }) => {
+  const [elPosition, setElPosition] = useState({ top: 0, left: 0 });
+  const [show, setShow] = useState(false);
+  let tooltipClass = 'tooltip';
+
+  const getPosition = (e) => {
+    const pos = e.currentTarget.getBoundingClientRect();
+
+    if(position === 'top') {
+      setElPosition({ top: pos.top + window.pageYOffset, left: pos.left + (pos.width / 2) + window.pageXOffset });
+    }
+    else if(position === 'bottom') {
+      setElPosition({ top: pos.bottom + window.pageYOffset, left: pos.left + (pos.width / 2) + window.pageXOffset });
+    }
+    else if(position === 'left') {
+      setElPosition({ top: pos.top + (pos.height / 2) + window.pageYOffset, left: pos.left + window.pageXOffset });
+    }
+    else if(position === 'right') {
+      setElPosition({ top: pos.top + (pos.height / 2) + window.pageYOffset, left: pos.left + pos.width + window.pageXOffset });
+    }
+    else if(position === 'top-left') {
+      setElPosition({ top: pos.top + window.pageYOffset, left: pos.left + window.pageXOffset });
+    }
+    else if(position === 'top-right') {
+      setElPosition({ top: pos.top + window.pageYOffset, left: pos.left + pos.width + window.pageXOffset });
+    }
+    else if(position === 'bottom-left') {
+      setElPosition({ top: pos.top + pos.height + window.pageYOffset, left: pos.left + window.pageXOffset });
+    }
+    else if(position === 'bottom-right') {
+      setElPosition({ top: pos.top + pos.height + window.pageYOffset, left: pos.left + pos.width + window.pageXOffset });
+    }
+    else if(position === 'left-top') {
+      setElPosition({ top: pos.top + window.pageYOffset, left: pos.left + window.pageXOffset });
+    }
+    else if(position === 'left-bottom') {
+      setElPosition({ top: pos.bottom + window.pageYOffset, left: pos.left + window.pageXOffset });
+    }
+    else if(position === 'right-top') {
+      setElPosition({ top: pos.top + window.pageYOffset, left: pos.left + pos.width + window.pageXOffset });
+    }
+    else if(position === 'right-bottom') {
+      setElPosition({ top: pos.bottom + window.pageYOffset, left: pos.left + pos.width + window.pageXOffset });
+    }
+
+    setShow(true);
+  }
+
+  if(position === 'top') {
+    tooltipClass += ' tooltip--top';
+  }
+  else if(position === 'bottom') {
+    tooltipClass += ' tooltip--bottom';
+  }
+  else if(position === 'left') {
+    tooltipClass += ' tooltip--left';
+  }
+  else if(position === 'right') {
+    tooltipClass += ' tooltip--right';
+  }
+  else if(position === 'top-left') {
+    tooltipClass += ' tooltip--top-left';
+  }
+  else if(position === 'top-right') {
+    tooltipClass += ' tooltip--top-right';
+  }
+  else if(position === 'bottom-left') {
+    tooltipClass += ' tooltip--bottom-left';
+  }
+  else if(position === 'bottom-right') {
+    tooltipClass += ' tooltip--bottom-right';
+  }
+  else if(position === 'left-top') {
+    tooltipClass += ' tooltip--left-top';
+  }
+  else if(position === 'left-bottom') {
+    tooltipClass += ' tooltip--left-bottom';
+  }
+  else if(position === 'right-top') {
+    tooltipClass += ' tooltip--right-top';
+  }
+  else if(position === 'right-bottom') {
+    tooltipClass += ' tooltip--right-bottom';
+  }
+
+  return(
+    <Fragment>
+      {show && <TooltipContent tooltipClass={tooltipClass} position={elPosition} content={content} tooltipPosition={position} duration={animationDuration} />}
+      {cloneElement(children, {...children.props, onMouseOver: getPosition, onMouseLeave: () => setShow(false)})}
+    </Fragment>
+  );
+}
+
+Tooltip.propTypes = {
+  children: PropTypes.element.isRequired,
+  content: PropTypes.any.isRequired,
+  position: PropTypes.oneOf(['top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left', 'bottom-right', 'left-top', 'left-bottom', 'right-top', 'right-bottom']).isRequired,
+  animationDuration: PropTypes.number
+}
+
+export default Tooltip;
